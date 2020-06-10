ff538bb8b8fbede464c3d9a466a85352429a7fbe
uemura-39 <k.u19970228@gmail.com>
2020-06-10 16:16:19 +0900

    部分テンプレートを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
     @posts = current_user.posts
-    @posts = user.posts.page(params[:page]).per(5).order("created_at DESC")
   end
 end
