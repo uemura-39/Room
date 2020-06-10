92f0735fac7a6559be65931992027ead5f3ff8cb
uemura-39 <k.u19970228@gmail.com>
2020-06-10 19:01:43 +0900

    かんたんログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
   has_many :posts      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :posts
   resources :users, only: :show
 end
