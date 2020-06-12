0cce6ee2cac9684a146d98bd87e6fd69963adab3
uemura-39 <k.u19970228@gmail.com>
2020-06-12 14:43:51 +0900

    ヘッダーを完成させました

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  resources :posts
+
+  resources :posts do
+    collection do
+      get 'search'
+    end
+  end
   resources :users, only: :show
 end
