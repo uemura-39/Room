276c97159eb1f957570a9502df5d18c88b979cb8
uemura-39 <k.u19970228@gmail.com>
2020-07-10 18:38:54 +0900

    privateセッティング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    redirect_to @user
   end
+
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :image, :profile)
+  end
+
 
 end
