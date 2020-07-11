77b0d8743a34b16c38ce9873877622648bbbd18b
uemura-39 <k.u19970228@gmail.com>
2020-07-11 10:18:17 +0900

    定数化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user
     else
@@ -17,10 +16,12 @@ class UsersController < ApplicationController
     end
   end
 
-
   def user_params
     params.require(:user).permit(:nickname, :email, :image, :profile)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 
 end
