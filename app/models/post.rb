a917e300e425be2be1bd4b5983035000a1f6d508
uemura-39 <k.u19970228@gmail.com>
2020-07-14 13:31:39 +0900

    liked_by?メソッド定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  # お気に入りされているか判定
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+
   validates :text, presence: true,length: { maximum: 200 }
   validates :image, presence: true
 
