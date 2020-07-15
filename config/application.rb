e7df55408be164bf873790e1268eb68ac775dd2c
uemura-39 <k.u19970228@gmail.com>
2020-07-15 21:20:06 +0900

    時間帯変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Room
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
 
     config.generators do |g|
       g.stylesheets false
