5ee187150bcce59d0c43c5d3dc7a66b2c6cf3e18
uemura-39 <k.u19970228@gmail.com>
2020-07-22 14:47:21 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'posts#index'
+  devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
