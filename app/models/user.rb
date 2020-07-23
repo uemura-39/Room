409ae9b27941ca29916703fdceffb4858d3503f2
uemura-39 <k.u19970228@gmail.com>
2020-07-23 22:12:18 +0900

    ゲストユーザー機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(nickname:"ゲスト",email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
   has_many :posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
